bf469d95564a5df6ab49a50901280eb451d1c7e9
GuND0Wn151 <58622550+GuND0Wn151@users.noreply.github.com>
2019-12-30 17:55:44 +0530

    Add files via upload

---
+++ 3d_space.py
@@ -0,0 +1,49 @@
+import turtle
+s = turtle.Turtle()
+s.color("red")
+s.speed(0)
+s.goto(0,0)
+s.fd(500)
+s.goto(0,0)
+s.bk(500)
+s.goto(0,0)
+s.left(90)
+s.fd(500)
+s.goto(0,0)
+s.bk(500)
+
+
+#y ot x
+for i in range(0,500,25):
+    s.penup()
+    s.goto(0,0)
+    s.pendown()
+    s.goto(0,i)
+    s.goto(500-i,0)
+#-y to -x
+for i in range(0,500,25):
+    s.penup()
+    s.goto(0,0)
+    s.pendown()
+    s.goto(0,-i)
+    s.goto(-(500-i),0)
+#x to -y
+for i in range(0,500,25):
+    s.penup()
+    s.goto(0,0)
+    s.pendown()
+    s.goto(i,0)
+    s.goto(0,-(500-i))
+
+for i in range(0,500,25):
+    s.penup()
+    s.goto(0,0)
+    s.pendown()
+    s.goto(0,i)
+    s.goto(-(500-i),0)
+
+
+
+
+
+turtle.done()
